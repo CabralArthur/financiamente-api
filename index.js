53f935ba091a16fbcb475124b7226c770bfefdb9
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-04-01 22:43:47 -0300

    Base para API utilizando express

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const api = express();
+
+api.use(express.json());
+
+api.post('/calcjs', (request, response) => {
+  //Pegando o corpo da requisição (o que for de campo dos formulários)
+  const { valIniJS, txJurJS, numPJS } = request.body;
+  let juros = eval(valIniJS * (txJurJS / 100) * numPJS);
+  let montante = eval(Number(valIniJS) + Number(juros));
+  response.json({ montante });
+});
+
+api.post('/calcjc', (request, response) => {
+  //Pegando o corpo da requisição (o que for de campo dos formulários)
+  const { valIniJS, txJurJS, numPJS } = request.body;
+  let juros = eval(valIniJS * (txJurJS / 100) * numPJS);
+  let montante = eval(Number(valIniJS) + Number(juros));
+  response.json({ montante });
+});
+
+api.post('/calcdesc', (request, response) => {
+  //Pegando o corpo da requisição (o que for de campo dos formulários)
+  const { valIniJS, txJurJS, numPJS } = request.body;
+  let juros = eval(valIniJS * (txJurJS / 100) * numPJS);
+  let montante = eval(Number(valIniJS) + Number(juros));
+  response.json({ montante });
+});
+
+api.listen(3000);
