b4b145c1ff6432d379d3897a7ec83222b998337f
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-04-02 20:48:58 -0300

    Finalizando Corpo da API

--- index.js
+++ index.js
@@ -13,19 +13,18 @@ api.post('/calcjs', (request, response) => {
 
 api.post('/calcjc', (request, response) => {
   //Pegando o corpo da requisição (o que for de campo dos formulários)
-  const { valIniJS, txJurJS, numPJS } = request.body;
-  //Trocar cálculos
-  let juros = eval(valIniJS * (txJurJS / 100) * numPJS);
-  let montante = eval(Number(valIniJS) + Number(juros));
+  const { valIniJC, txJurJC, numPJC } = request.body;
+  let montante = eval(
+    Number(valIniJC) * Math.pow(1 + Number(txJurJC / 100), numPJC),
+  );
   response.json({ montante });
 });
 
 api.post('/calcdesc', (request, response) => {
   //Pegando o corpo da requisição (o que for de campo dos formulários)
-  const { valIniJS, txJurJS, numPJS } = request.body;
-  //Trocar cálculos
-  let juros = eval(valIniJS * (txJurJS / 100) * numPJS);
-  let montante = eval(Number(valIniJS) + Number(juros));
+  const { valDesej, Desc } = request.body;
+  let desc = eval(valDesej * (Desc / 100));
+  let montante = eval(Number(valDesej) - Number(desc));
   response.json({ montante });
 });
 
