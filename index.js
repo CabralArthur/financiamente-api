977b513e849270a86a88e257929f2b35e77d5870
AhCabral <arthurcabral.dev@gmail.com>
2021-04-05 10:04:51 -0300

    Reformulando código

--- index.js
+++ index.js
@@ -6,29 +6,36 @@ const api = express();
 api.use(cors());
 api.use(express.json());
 
-api.post('/calcjs', (request, response) => {
+
+//Para essa API serão utilizadas 3 rotas diferentes
+
+//A primeira rota, para o cálculo de Juros simples
+api.get('/calcjs', (request, response) => {
   //Pegando o corpo da requisição (o que for de campo dos formulários)
-  const { valIniJS, txJurJS, numPJS } = request.body;
+  const { valIniJS, txJurJS, numPJS } = request.params;
   let juros = Number(valIniJS) * (Number(txJurJS) / 100) * Number(numPJS);
   let montante = Number(valIniJS) + Number(juros);
   response.json({ montante });
 });
 
-api.post('/calcjc', (request, response) => {
+//A segunda, para cálculo de juros compostos
+api.get('/calcjc', (request, response) => {
   //Pegando o corpo da requisição (o que for de campo dos formulários)
-  const { valIniJC, txJurJC, numPJC } = request.body;
+  const { valIniJC, txJurJC, numPJC } = request.params;
   let montante = eval(
     Number(valIniJC) * Math.pow(1 + Number(txJurJC / 100), numPJC),
   );
   response.json({ montante });
 });
 
-api.post('/calcdesc', (request, response) => {
+//A terceira para cálculo de descontos
+api.get('/calcdesc', (request, response) => {
   //Pegando o corpo da requisição (o que for de campo dos formulários)
-  const { valDesej, Desc } = request.body;
+  const { valDesej, Desc } = request.params;
   let desc = eval(valDesej * (Desc / 100));
   let montante = eval(Number(valDesej) - Number(desc));
   response.json({ montante });
 });
 
+//API será "ouvida" na porta 3000 do navegador
 api.listen(3000);
